dec47915f24cb595a127ef2d7e8ac5ceb79b0627
Curtis Pritchard <cpritchard649@gmail.com>
2022-04-04 20:09:28 -0400

    * .gitignore: Initial Commit
    * code_locker.sln:
    * Program.cs:
    * code_locker.csproj:
    * code_locker.csproj.user:
    * AssemblyInfo.cs:

---
+++ code_locker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace code_locker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
